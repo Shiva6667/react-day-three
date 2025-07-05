76514a865298587a0096c6c3660c1054a591ddc8
Shiva6667 <mshiva61008@gmail.com>
2025-07-05 20:03:08 +0530

    Added Git Workflow.

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+
+
+import './App.css';
+import Chart from 'react-google-charts';
+import HomePageCarousel from './components/carousel/carousel.component';
+import MainNavbar from './components/navbar/navbar.component';
+function App() {
+
+  const chartData = {
+      
+  "cols": [
+    {"id": "", "label": "Language", "pattern": "", "type": "string"},
+    {"id": "", "label": "Popularity", "pattern": "", "type": "number"}
+  ],
+  "rows": [
+    {"c": [{"v": "Python"}, {"v": 29.9}]},
+    {"c": [{"v": "JavaScript"}, {"v": 19.1}]},
+    {"c": [{"v": "Java"}, {"v": 15.2}]},
+    {"c": [{"v": "C#"}, {"v": 9.8}]},
+    {"c": [{"v": "C++"}, {"v": 7.4}]},
+    {"c": [{"v": "TypeScript"}, {"v": 6.2}]},
+    {"c": [{"v": "PHP"}, {"v": 5.1}]},
+    {"c": [{"v": "Go"}, {"v": 3.7}]},
+    {"c": [{"v": "Ruby"}, {"v": 1.8}]},
+    {"c": [{"v": "Swift"}, {"v": 1.5}]}
+  ]
+
+
+  }
+  
+
+  return (
+    <>
+      <MainNavbar/>
+      <HomePageCarousel/>
+      <div className='container'>
+        <div className='row'>
+          <div className='col'>
+            <Chart
+      // Try different chart types by changing this property with one of: LineChart, BarChart, AreaChart...
+      chartType="PieChart"
+      data={chartData}
+      options={{
+        title: "Average Weight by Age",
+      }}
+      height={"400px"}
+      legendToggle
+    />
+    
+          </div>
+          <div className='col'>
+            Col2
+          </div>
+        </div>
+        
+      </div>
+  
+    </>
+  )
+}
+
+export default App
---
+++ src/components/carousel/carouselImage.component.jsx
@@ -0,0 +1,11 @@
+export default function CarouselImage(){
+    return <div style={{
+        backgroundColor:"gray",
+        height:"300px"
+
+
+
+}}>
+        Hello World
+    </div>
+}
